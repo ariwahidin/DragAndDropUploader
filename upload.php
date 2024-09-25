7a537f746d9fd28641e50778bb25a743bd9e40bd
Ari Wahidin <ari.wahidin@PYIDJKT.local>
2024-09-25 17:47:08 +0700

    disable upload function

--- upload.php
+++ upload.php
@@ -13,29 +13,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $totalFiles = count($files['name']);
 
         // Loop untuk memproses semua file yang di-upload
-        for ($i = 0; $i < $totalFiles; $i++) {
-            $fileName = basename($files['name'][$i]);
-            $fileTmpName = $files['tmp_name'][$i];
-            $fileSize = $files['size'][$i];
-            $fileError = $files['error'][$i];
+        // for ($i = 0; $i < $totalFiles; $i++) {
+        //     $fileName = basename($files['name'][$i]);
+        //     $fileTmpName = $files['tmp_name'][$i];
+        //     $fileSize = $files['size'][$i];
+        //     $fileError = $files['error'][$i];
 
-            // Cek jika ada error saat upload file
-            if ($fileError === UPLOAD_ERR_OK) {
-                // Set path untuk menyimpan file
-                $fileDestination = $uploadDir . $fileName;
+        //     // Cek jika ada error saat upload file
+        //     if ($fileError === UPLOAD_ERR_OK) {
+        //         // Set path untuk menyimpan file
+        //         $fileDestination = $uploadDir . $fileName;
 
-                // Pindahkan file yang di-upload ke folder tujuan
-                if (move_uploaded_file($fileTmpName, $fileDestination)) {
-                    $uploadedFiles[] = $fileName; // Tambahkan nama file ke daftar file yang berhasil di-upload
-                } else {
-                    echo json_encode(['error' => 'Failed to upload file: ' . $fileName]);
-                    exit;
-                }
-            } else {
-                echo json_encode(['error' => 'Error uploading file: ' . $fileName]);
-                exit;
-            }
-        }
+        //         // Pindahkan file yang di-upload ke folder tujuan
+        //         if (move_uploaded_file($fileTmpName, $fileDestination)) {
+        //             $uploadedFiles[] = $fileName; // Tambahkan nama file ke daftar file yang berhasil di-upload
+        //         } else {
+        //             echo json_encode(['error' => 'Failed to upload file: ' . $fileName]);
+        //             exit;
+        //         }
+        //     } else {
+        //         echo json_encode(['error' => 'Error uploading file: ' . $fileName]);
+        //         exit;
+        //     }
+        // }
 
         // Jika semua file berhasil di-upload, kirimkan respons sukses
         echo json_encode(['success' => true, 'uploadedFiles' => $uploadedFiles]);
